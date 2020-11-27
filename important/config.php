8d7930b0bc54d67320c80e725e821ef3d23d9953
martincodes <62392843+martincodes-de@users.noreply.github.com>
2020-11-27 22:20:01 +0100

    hinzufügen von funktionierender config und seitentemplate

---
+++ important/config.php
@@ -0,0 +1,28 @@
+<?php
+
+# Allgemeine Einstellungen
+
+$site_fullname = "LSPD: Commanding Officer Dashboard";
+$site_shortname = "LSPD: CO-Dashboard";
+$site_version = "[DEV] 27.11.2020";
+$site_programmer = "martincodes";
+
+# Datenbank Einstellungen
+
+$db_host = "localhost";
+$db_name = "lspd-co-dashboard";
+$db_username = "root";
+$db_userpassword = "";
+$db_charset = "utf8";
+
+# Try-Catch-Verbindung + Sicherheitsfeature
+
+try {
+  $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=$db_charset", $db_username, $db_userpassword);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch (PDOException $e) {
+  echo 'Datenbankverbindung fehlgeschlagen: ' . $e->getMessage();
+}
+
+# Klassen- und Funktionseinbindung
---
+++ pages/template.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("standards/header.php");
+require_once("standards/menu.php");
+
+?>
+
+<h1 class="mt-4">Static Navigation</h1>
+
+<div class="row">
+
+</div>
+
+<?php
+
+require_once("standards/footer.php");
+
+?>
